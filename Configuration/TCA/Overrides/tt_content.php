20be7c2cdcb0189ae2e82ca7d87195455955de45
Maik Kempe <mkempe@bitaculous.com>
2018-09-20 13:49:30 +0200

    [♻] Cleaned up.

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -8,13 +8,14 @@ $extensionKey        = 't3v_navigations';
 $extensionIdentifier = \T3v\T3vCore\Utility\ExtensionUtility::extensionIdentifier($extensionKey);
 $extensionSignature  = \T3v\T3vCore\Utility\ExtensionUtility::extensionSignature($namespace, $extensionKey);
 $flexFormsFolder     = \T3v\T3vCore\Utility\ExtensionUtility::flexFormsFolder($extensionKey);
+$lll                 = \T3v\T3vCore\Utility\ExtensionUtility::lll($extensionKey, 'locallang_ttc.xlf');
 
 // === Content Elements ===
 
 // --- Overview Navigation Content Element ---
 
 $contentElementKey         = 'Overview Navigation';
-$contentElementDescription = 'Overview Navigation Content Element';
+$contentElementDescription = $lll . 'contentElements.overviewNavigationContentElement.description';
 $contentElementIdentifier  = \T3v\T3vContent\Utility\ContentElementUtility::contentElementIdentifier($contentElementKey);
 $contentElementSignature   = \T3v\T3vContent\Utility\ContentElementUtility::contentElementSignature($extensionIdentifier, $contentElementIdentifier);
 
@@ -31,7 +32,7 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$contentEleme
 // --- Quick Navigation Content Element ---
 
 $contentElementKey         = 'Quick Navigation';
-$contentElementDescription = 'Quick Navigation Content Element';
+$contentElementDescription = $lll . 'contentElements.quickNavigationContentElement.description';
 $contentElementIdentifier  = \T3v\T3vContent\Utility\ContentElementUtility::contentElementIdentifier($contentElementKey);
 $contentElementSignature   = \T3v\T3vContent\Utility\ContentElementUtility::contentElementSignature($extensionIdentifier, $contentElementIdentifier);
 
@@ -45,10 +46,10 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$contentE
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$contentElementSignature] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($contentElementSignature, "{$flexFormsFolder}/ContentElements/QuickNavigationContentElement.xml");
 
-// --- Quick Navigation Content Element ---
+// --- Subpages Navigation Content Element ---
 
 $contentElementKey         = 'Subpages Navigation';
-$contentElementDescription = 'Subpages Navigation Content Element';
+$contentElementDescription = $lll . 'contentElements.subpagesNavigationContentElement.description';
 $contentElementIdentifier  = \T3v\T3vContent\Utility\ContentElementUtility::contentElementIdentifier($contentElementKey);
 $contentElementSignature   = \T3v\T3vContent\Utility\ContentElementUtility::contentElementSignature($extensionIdentifier, $contentElementIdentifier);
 
--- TODOS.md
+++ TODOS.md
@@ -1,5 +1,4 @@
 TODOS
 =====
 
-- [ ] Clean up TypoScript, move all to `plugin.tx_t3vnavigations`
 - [ ] Implement `Social` Navigation
