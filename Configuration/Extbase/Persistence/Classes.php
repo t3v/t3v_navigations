fb2f2fb479a686015a527f446486a1f01da2f5c2
Jochen Greiner <jochen@greiner.de>
2020-11-25 11:32:01 +0100

    typo310 vorbereitung: Breaking: #87623 - Replace config.persistence.classes typoscript configuration

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+return [
+    \T3v\T3vNavigations\Domain\Model\FlyoutNavigationItem::class => [
+        'tableName' => 'tx_t3vnavigations_domain_model_flyout_navigation_item'
+    ],
+    \T3v\T3vNavigations\Domain\Model\OverviewNavigationItem::class => [
+        'tableName' => 'tx_t3vnavigations_domain_model_overview_navigation_item'
+    ],
+    \T3v\T3vNavigations\Domain\Model\QuickNavigationItem::class => [
+        'tableName' => 'tx_t3vnavigations_domain_model_quick_navigation_item'
+    ],
+    \T3v\T3vNavigations\Domain\Model\VendorNavigationItem::class => [
+        'tableName' => 'tx_t3vnavigations_domain_model_vendor_navigation_item'
+    ]
+];
